03a350282b28f693911767f4c855eb5372aa979c
jpacsai <judit.pacsai@gmail.com>
2018-09-19 23:29:48 +0200

    style: display
    add basic layout, border, icons

--- src/Display.js
+++ src/Display.js
@@ -6,10 +6,16 @@ class Display extends Component {
   render() {
     return (
         <div id='timer-container'>
+          <div id='time-left'>5</div>
+          <div id='timer-container-buttons'>
+            <button id='start_stop' className='control-btn'>
+            <i className="fas fa-pause"></i>
+            </button>
+            <button id='reset' className='control-btn'>
+              <i className="fas fa-redo"></i>
+            </button>
+          </div>
           <h2 id='timer-label'>Session</h2>
-          <div id='time-left'></div>
-          <button id='start_stop'></button>
-          <button id='reset'></button>
         </div>
     );
   }
--- src/LengthControl.js
+++ src/LengthControl.js
@@ -9,24 +9,24 @@ class LengthControl extends Component {
           <section id='break-container'>
             <h2 id='break-label'>Break length</h2>
             <div id='break-set' className='length-set'>
-              <button id='break-decrement' className='length-control-btn'>
-                <i class="fas fa-angle-down"></i>
+              <button id='break-decrement' className='control-btn'>
+                <i className="fas fa-angle-down"></i>
               </button>
               <div id='break-length' className='length-display'>5</div>
-              <button id='break-increment' className='length-control-btn'>
-                <i class="fas fa-angle-up"></i>
+              <button id='break-increment' className='control-btn'>
+                <i className="fas fa-angle-up"></i>
               </button>
             </div>
           </section>
           <section id='session-container'>
             <h2 id='session-label'>Session length</h2>
             <div id='session-set'  className='length-set'>
-              <button id='session-decrement' className='length-control-btn'>
-                <i class="fas fa-angle-down"></i>
+              <button id='session-decrement' className='control-btn'>
+                <i className="fas fa-angle-down"></i>
               </button>
               <div id='session-length' className='length-display'>25</div>
-              <button id='session-increment' className='length-control-btn'>
-                <i class="fas fa-angle-up"></i>
+              <button id='session-increment' className='control-btn'>
+                <i className="fas fa-angle-up"></i>
               </button>
             </div>
           </section>
