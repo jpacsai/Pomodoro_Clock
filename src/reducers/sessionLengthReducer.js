e9642e01a5a9d0fcdc348ee5eb798f90ba2a9ccd
jpacsai <judit.pacsai@gmail.com>
2018-09-20 23:28:49 +0200

    fix: add limits to session length 1-60

--- src/reducers/sessionLengthReducer.js
+++ src/reducers/sessionLengthReducer.js
@@ -2,10 +2,10 @@ export default function sessionLengthReducer(state = 25, action) {
     
     switch(action.type) {
         case 'INCREMENT_SESSION':
-            return ++state;
+            return state + 1 > 60 ? 60 : ++state;
 
         case 'DECREMENT_SESSION':
-            return --state;
+            return state - 1 === 0 ? 1 : --state;
 
         default:
             return state;
