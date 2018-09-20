208eaaaa060802e195e360143e58fec5b04e61c1
jpacsai <judit.pacsai@gmail.com>
2018-09-20 23:27:52 +0200

    fix: add limits to break length 1-60

--- src/reducers/breakLengthReducer.js
+++ src/reducers/breakLengthReducer.js
@@ -2,10 +2,10 @@ export default function breakLengthReducer(state = 5, action) {
     console.log(action.type)
     switch(action.type) {
         case 'INCREMENT_BREAK':
-            return ++state;
+            return state + 1 > 60 ? 60 : ++state;
 
         case 'DECREMENT_BREAK':
-            return --state;
+            return state - 1 === 0 ? 1 : --state;
 
         default:
             return state;
