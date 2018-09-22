1ae4b7c3b6e65636448a6a2d234ad82f64a9c204
jpacsai <judit.pacsai@gmail.com>
2018-09-22 15:13:53 +0200

    fix: break length set buttons incr/decr break length set display

--- src/reducers/breakLengthReducer.js
+++ src/reducers/breakLengthReducer.js
@@ -1,10 +1,12 @@
 export default function breakLengthReducer(state = 5, action) {
 
     switch(action.type) {
-        case 'INCREMENT_BREAK':
+        case 'INCREMENT_BREAK_TRUE':
+        case 'INCREMENT_BREAK_FALSE':
             return state + 1 > 60 ? 60 : ++state;
 
-        case 'DECREMENT_BREAK':
+        case 'DECREMENT_BREAK_TRUE':
+        case 'DECREMENT_BREAK_FALSE':
             return state - 1 === 0 ? 1 : --state;
 
         case 'RESET':
